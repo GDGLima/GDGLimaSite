a09c331b6c2b4f36477282a550c1e15c755e93fd
Manuel <memo881@gmail.com>
2012-11-07 12:34:35 -0500

    Adding image Optimization

--- grunt.js
+++ grunt.js
@@ -6,21 +6,21 @@ module.exports = function(grunt) {
 	// https://github.com/cowboy/grunt/blob/master/docs/getting_started.md
 	//
 	grunt.initConfig({
-		img: {
-			task2: {
-				src: 'include/images/**/*.*',
-				dest: 'build/include/images'
-			}
-		},
 		min: {
 			dist: {
 				src: ['include/js/jscript.js', 'include/js/jscanvas.js'],
 				dest: 'build/include/js/build.min.js',
 				separator: ';'
 			}
+		},
+		smushit:{
+			dir:{
+				src:'include/images/',
+				dest:'build/include/images/'
+			}
 		}
 	});
-	grunt.loadNpmTasks('grunt-img');
-	grunt.registerTask('default', 'min img');
+	grunt.loadNpmTasks('grunt-smushit');
+	grunt.registerTask('default', 'min smushit');
 
 };
