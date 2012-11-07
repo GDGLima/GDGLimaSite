93aa24ec7208a772e2ccbe200f4bed79426f6ad2
Manuel <memo881@gmail.com>
2012-11-07 13:53:29 -0500

    Changing grunt for minifications

--- README.md
+++ README.md
@@ -3,6 +3,19 @@ DevFestLimaSite
 
 Este es el codigo fuente del site del GDG LIma para el DevfestLima que se realizo el 27 y 28 de octubre del 2012
 
+
+##Building
+Actualmente el codigo funciona como esta, pero es recomendado utilizar [Grunt](http://gruntjs.com/) para la optimizacion del codigo (minificaciones, optimizacion de imagenes, etc)
+
+
+##Dependencias
+Para poder utilizar el archivo de grunt.js se necesita tener los siguientes modulos
+* [grunt-smushit](https://github.com/heldr/grunt-smushit)
+* [grunt-contrib](https://github.com/gruntjs/grunt-contrib)
+* [grunt-recess](https://github.com/sindresorhus/grunt-recess)
+Solamente hay un task (Default) asi que con grunt (grunt.cmd en Windows) se puede compilar
+
+
 ##Autores
 La primera version de este proyecto es un esfuerzo conjunto de las siguientes personas
 * [Carlos Piñan] (http://www.carlospinan.com/)
--- grunt.js
+++ grunt.js
@@ -6,21 +6,46 @@ module.exports = function(grunt) {
 	// https://github.com/cowboy/grunt/blob/master/docs/getting_started.md
 	//
 	grunt.initConfig({
+		copy: {
+			dist: {
+				files: {
+					"build/include/": "include/**",
+					"build/":["icono.ico"]
+				}
+			}
+		},
 		min: {
 			dist: {
-				src: ['include/js/jscript.js', 'include/js/jscanvas.js'],
+				src: [	'include/js/jquery.min.js',
+						'include/js/easeljs-0.5.0.min.js',
+						'include/js/jscript.js',
+						'include/js/jscanvas.js',
+						'include/js/moderator-api.js'],
 				dest: 'build/include/js/build.min.js',
 				separator: ';'
 			}
 		},
-		smushit:{
-			dir:{
-				src:'include/images/',
-				dest:'build/include/images/'
+		recess: {
+			dist: {
+				src: ['include/css/styles.css','include/css/reset.css'],
+				dest: 'build/include/css/styles.css',
+				options: {
+					compile: true,
+					compress:true
+				}
+			}
+		},
+		smushit: {
+			dir: {
+				src: 'include/images/',
+				dest: 'build/include/images/'
 			}
 		}
+
 	});
+	grunt.loadNpmTasks('grunt-contrib');
+	grunt.loadNpmTasks('grunt-recess');
 	grunt.loadNpmTasks('grunt-smushit');
-	grunt.registerTask('default', 'min smushit');
+	grunt.registerTask('default', 'copy min recess smushit');
 
 };
